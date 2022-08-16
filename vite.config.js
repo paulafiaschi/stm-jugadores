dedc88157d9bb26213b05b89d838ce81a454c1f3
paulafiaschi <paul6399@stud.kea.dk>
2022-08-16 15:59:16 +0200

    redirects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
+import ssr from "vite-plugin-ssr/plugin";
 import react from "@vitejs/plugin-react";
+import { version } from "os";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
 });
